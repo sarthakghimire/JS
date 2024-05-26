683a64ceb369f4ce0c9fb3af5fe1ee6b0916fa35
sarthakghimire <sarthakghimire.sg@gmail.com>
2024-05-26 21:43:45 +0545

    javascript.info

---
+++ 2.13.js
@@ -0,0 +1,20 @@
+//Loops
+//while loop
+let a=0;
+while(a<=10){
+    console.log(a);
+    a++;
+}
+
+//for loop
+a=0;
+for(;a<=10;a++){
+    console.log(a);
+}
+
+//do...while loop
+a=0;
+do{
+    console.log(a);
+    a++
+}while(a<=10);
